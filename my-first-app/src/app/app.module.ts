52f95b61c2ec6b338d2f2c66264d17d02a5b4cd6
Matthew Allen <mallenswe@gmail.com>
2019-11-10 21:19:19 -0800

    Component Assignment
    Assignment: Create two component, warning-alert and success-alert and add them to the app.component.html. Style them to look like warning and success alerts.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './alerts/success/success.component';
+import { WarningComponent } from './alerts/warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
