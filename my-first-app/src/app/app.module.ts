0620c764c7e333016faee8aa24e242bde6ea5d54
Matthew Allen <mallenswe@gmail.com>
2019-11-10 21:39:58 -0800

    String Interpolation and Property Binding
    Adding string interpolation and property binding to the server and servers component to disable and enable a button as well as show a list of servers

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessComponent } from './alerts/success/success.component';
 import { WarningComponent } from './alerts/warning/warning.component';
+import { DangerComponent } from './alerts/danger/danger.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { WarningComponent } from './alerts/warning/warning.component';
     ServerComponent,
     ServersComponent,
     SuccessComponent,
-    WarningComponent
+    WarningComponent,
+    DangerComponent
   ],
   imports: [
     BrowserModule,
