5e092910b93a54710084ab1d3e7781253e09fe88
Matthew Allen <mallenswe@gmail.com>
2019-11-12 20:57:00 -0800

    Course Project Basics: Recipe, Recipe List, Shopping List
    Added layout for Recipes, Recipe List, and Shopping List.
    Added models for Recipe and Ingredients.

--- my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test', 'This is a test', 'https://live.staticflickr.com/4607/39683662521_0b5d5d383e_k.jpg'),
+    new Recipe('Test', 'This is a test', 'https://live.staticflickr.com/4607/39683662521_0b5d5d383e_k.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
