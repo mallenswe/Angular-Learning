bb45831251dda994f58b4158ee5a6e64b074a154
Matthew Allen <mallenswe@gmail.com>
2019-12-04 20:03:25 -0800

    Course Project Components & Data Binding
    Recipes now display in their own component when selected and users can now add ingredients from the shopping page.

--- my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipe } from '../recipe.model';
 
@@ -8,13 +8,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test', 'This is a test', 'https://live.staticflickr.com/4607/39683662521_0b5d5d383e_k.jpg'),
-    new Recipe('Test', 'This is a test', 'https://live.staticflickr.com/4607/39683662521_0b5d5d383e_k.jpg')
+    new Recipe('Another', 'One test', 'https://live.staticflickr.com/4607/39683662521_0b5d5d383e_k.jpg')
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
